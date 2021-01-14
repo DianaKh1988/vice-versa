27884bf1612d716118d93ac626026b94f0b1810a
Diana <dianakhromova88@gmail.com>
2021-01-14 18:47:21 +0300

    We have reversed text

--- viceversa/views.py
+++ viceversa/views.py
@@ -7,6 +7,11 @@ def main(request):
 
 def reverse(request):
 	user_text = request.GET['usertext']
+	count_word = 0
+	count_word = len(user_text.split())  
 	reversed_text = user_text[::-1]
-	return render(request, 'reverse.html', {'usertext':user_text,
-		'reversedtext':reversed_text})
+	return render(request, 'reverse.html', {
+		'usertext':user_text,
+		'reversedtext':reversed_text,
+		'countword': count_word
+	})
